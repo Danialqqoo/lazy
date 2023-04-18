d008db81c81ac29bb89822b0cc2631d761e33c55
Lazy.dli <rolan.aman228@gmail.com>
2023-04-19 02:14:55 +0600

    update

--- server.js
+++ server.js
@@ -5,6 +5,45 @@ const app = express();
 
 app.use(express.static('public'));
 
+
+var sqlite3 = require('sqlite3').verbose()
+let db = new sqlite3.Database('database.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
+
+
+
+
+
+
+  
+db.serialize(function () {
+  db.run("CREATE TABLE if not exists admin(id INTEGER PRIMARY KEY AUTOINCREMENT, login TEXT, password TEXT)");
+  
+  
+
+ 
+ 
+ 
+ 
+ 
+//   db.each('SELECT rowid AS id, info FROM lorem', function (
+//     err,
+//     row
+//   ) {
+//     console.log(row.id + ': ' + row.info)
+//   })
+})
+
+db.close()
+
+
+
+
+
 // определяем обработчик для маршрута "/"
 app.get("/", function(request, response){
 
@@ -14,7 +53,7 @@ app.get("/", function(request, response){
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
 
-////d;lskgsd;lk
+
 
 app.get("/about", function(request, response){
     response.sendFile(path.join(__dirname, '/public', 'admin.html'));
